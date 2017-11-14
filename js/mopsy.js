fa2a7dbceb091edefd8547e591c9f6cbc05b1f44
mopsy <mopsyshin@gmail.com>
2017-11-14 22:25:21 +0900

    undo

---
+++ js/mopsy.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+
+  $("#home").animate({
+    opacity: 1,
+    top: 0
+  }, 800, 'swing');
+
+
+  $(".wrapper").animate({
+    opacity: 1,
+    left: 0
+  }, 800,'swing', function(){
+    $("#profile-textbox").animate({
+      opacity: 1,
+      top: 0
+    }, 200, 'swing');
+    $("#detail-content").animate({
+       opacity: 1,
+       left: 0
+     }, 200, 'swing'
+    );
+    $("#link-fb, #link-brunch").animate({
+      opacity: 1,
+      bottom: 0
+    }, 200, 'swing');
+  });
+
+
+
+  $("#home").click(function(){
+    $(location).attr('href', './index.html');
+  });
+
+  $("#link-fb").click(function(){
+    window.open('https://www.facebook.com/mopsyshin', '_blank');
+  });
+
+  $("#link-brunch").click(function(){
+    window.open('https://brunch.co.kr/@mopsyshin', '_blank');
+  });
+
+
+});
