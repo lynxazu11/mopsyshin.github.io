d539189c9a1c0cf96e2df5d921334cb5641b1055
mopsy <mopsyshin@gmail.com>
2017-11-17 15:10:26 +0900

    refactoring

--- components/portfolio/portfolio.js
+++ components/portfolio/portfolio.js
@@ -1,72 +1,23 @@
+$(document).ready(function(){
+    portfolioFunc();
+})
 
-$("#redot").click(function(){
-    var url = "./components/portfolio/redot.html"; 
-    $(".wrapper").addClass("inactive");
-    setTimeout(function(){
-        $("#app").load(url);
-    }, 300);
-});
+function pageReverse( id ) {
+    var btn = "#" + id ;
+    var url =  "./components/portfolio/" + id + ".html";
+    $(btn).click(function(){
+        $(".wrapper").addClass("inactive");
+        setTimeout(function(){
+            $("#app").load(url);
+        }, 300);
+    });
+}
 
-$("#08l").click(function(){
-    var url = "./components/portfolio/08l.html"; 
-    $(".wrapper").addClass("inactive");
-    setTimeout(function(){
-        $("#app").load(url);
-    }, 300);
-});
+function portfolioFunc() {
+    var itemLength = $(".item-portfolio").length;
+    for ( var i=0; i<itemLength; i++ ) {
+        var id = $('.item-portfolio')[i].id;
+        pageReverse( id );        
+    }
+}
 
-$("#mosher").click(function(){
-    var url = "./components/portfolio/mosher.html"; 
-    setTimeout(function(){
-        $("#app").load(url);
-    }, 300);
-});
-
-$("#bbuzzart").click(function(){
-    var url = "./components/portfolio/bbuzzart.html"; 
-    setTimeout(function(){
-        $("#app").load(url);
-    }, 300);
-});
-
-$("#caricature").click(function(){
-    var url = "./components/portfolio/caricature.html"; 
-    setTimeout(function(){
-        $("#app").load(url);
-    }, 300);
-});
-
-$("#anno").click(function(){
-    var url = "./components/portfolio/anno.html"; 
-    setTimeout(function(){
-        $("#app").load(url);
-    }, 300);
-});
-
-$("#player").click(function(){
-    var url = "./components/portfolio/player.html"; 
-    setTimeout(function(){
-        $("#app").load(url);
-    }, 300);
-});
-
-$("#profile-list").click(function(){
-    var url = "./components/portfolio/profile-list.html"; 
-    setTimeout(function(){
-        $("#app").load(url);
-    }, 300);
-});
-
-$("#emergency").click(function(){
-    var url = "./components/portfolio/emergency.html"; 
-    setTimeout(function(){
-        $("#app").load(url);
-    }, 300);
-});
-
-$("#republic").click(function(){
-    var url = "./components/portfolio/republic.html"; 
-    setTimeout(function(){
-        $("#app").load(url);
-    }, 300);
-});
