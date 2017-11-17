cd64d771dacd60108ab5a941221ba48f8fe0de03
mopsy <mopsyshin@gmail.com>
2017-11-17 16:53:51 +0900

    load animation 추

--- components/portfolio/portfolio.js
+++ components/portfolio/portfolio.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     portfolioFunc();
+    timeOutFunc()
 })
 
 function pageReverse( id ) {
@@ -21,3 +22,20 @@ function portfolioFunc() {
     }
 }
 
+function activeItem ( e ){
+    var activeId = "#" + e;
+    $(activeId).addClass("active");
+}
+
+function timeOutFunc() {
+    var itemLength = $(".item").length;
+    for (var i =0; i<itemLength; i++ ) {
+        (function(index) {
+            var findId = $('.item')[i].id;
+            setTimeout(function() { 
+                activeItem ( findId );
+            }, i * 70);
+        })(i);
+        
+    }
+}
