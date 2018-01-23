b89a12133a9949e3e546aec053fe306aba071b25
mopsy <mopsyshin@gmail.com>
2018-01-23 20:11:57 +0900

    animation adjust

--- heartclick/js/main.js
+++ heartclick/js/main.js
@@ -2,6 +2,7 @@ $(".btn").click(function(){
     var len = $(".img").length;  
     for ( var i = 0; i < len; i++ ) {
         if ( $(".img" + i).hasClass("active") !== true) {
+            $(".img" + (i - 1)).addClass("inactive");
             $(".img" + i).addClass("active");
             $(".counter")[0].textContent = i;
             break;
@@ -13,8 +14,9 @@ $(".btn").click(function(){
 });
 $(".refresh").click(function(){
     var len = $(".img").length;  
-    for ( var i = 1; i < len; i++ ) {
-            $(".img" + i).removeClass("active");
+    for ( var i = 0; i < len; i++ ) {
+            $(".img" + (i + 1)).removeClass("active");
+            $(".img" + i).removeClass("inactive");
     }
     $(this).removeClass("active");
     $(".counter")[0].textContent = 0;
